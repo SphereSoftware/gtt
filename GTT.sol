5cd4f07e325ab86403262206e3e3dc1316918a55
Anton Shemerey <shemerey@gmail.com>
2019-11-26 12:26:50 +0300

    add some details here

---
+++ GTT.sol
@@ -0,0 +1,138 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/access/roles/MinterRole.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ * https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+ * Originally based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ *
+ * This implementation emits additional Approval events, allowing applications to reconstruct the allowance status for
+ * all accounts just by listening to said events. Note that this isn't required by the specification, and other
+ * compliant implementations may not do it.
+ */
+contract GTT is IERC20, Ownable, MinterRole {
+    using SafeMath for uint256;
+    mapping (address => uint256) private _balances;
+    mapping (address => mapping (address => uint256)) private _allowed;
+    uint256 private _totalSupply;
+    string public constant name = "GettCoin";
+    string public constant symbol = "GTT";
+    uint32 public constant decimals = 18;
+    uint256 public constant tokenCap = 2**32;
+
+    constructor() public {
+        _totalSupply = _totalSupply.add(tokenCap);
+        _balances[msg.sender] = _balances[msg.sender].add(tokenCap);
+        emit Transfer(address(this), msg.sender, tokenCap);
+    }
+
+    /**
+    * @dev Total number of tokens in existence
+    */
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+    * @dev Gets the balance of the specified address.
+    * @param owner The address to query the balance of.
+    * @return An uint256 representing the amount owned by the passed address.
+    */
+    function balanceOf(address owner) public view returns (uint256) {
+        return _balances[owner];
+    }
+
+    /**
+     * @dev Function to check the amount of tokens that an owner allowed to a spender.
+     * @param owner address The address which owns the funds.
+     * @param spender address The address which will spend the funds.
+     * @return A uint256 specifying the amount of tokens still available for the spender.
+     */
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowed[owner][spender];
+    }
+
+    /**
+    * @dev Transfer token for a specified address
+    * @param to The address to transfer to.
+    * @param value The amount to be transferred.
+    */
+    function transfer(address to, uint256 value) public returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+
+    /**
+     * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+     * Beware that changing an allowance with this method brings the risk that someone may use both the old
+     * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+     * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     * @param spender The address which will spend the funds.
+     * @param value The amount of tokens to be spent.
+     */
+    function approve(address spender, uint256 value) public returns (bool) {
+        require(spender != address(0));
+        _allowed[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    /**
+     * @dev Transfer tokens from one address to another.
+     * @param from address The address which you want to send tokens from
+     * @param to address The address which you want to transfer to
+     * @param value uint256 the amount of tokens to be transferred
+     * Note: This operation can be done only by 'owner'
+     */
+    function transferFrom(address from, address to, uint256 value) public onlyOwner returns (bool) {
+        _transfer(from, to, value);
+        return true;
+    }
+
+    /**
+    * @dev Transfer token for a specified addresses
+    * @param from The address to transfer from.
+    * @param to The address to transfer to.
+    * @param value The amount to be transferred.
+    */
+    function _transfer(address from, address to, uint256 value) internal onlyOwner {
+        require(to != address(0));
+
+        _balances[from] = _balances[from].sub(value);
+        _balances[to] = _balances[to].add(value);
+        emit Transfer(from, to, value);
+    }
+
+    /**
+     * @dev Function to mint tokens
+     * @param to The address that will receive the minted tokens.
+     * @param value The amount of tokens to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function mint(address to, uint256 value) public onlyMinter returns (bool) {
+        _mint(to, value);
+        return true;
+    }
+
+    /**
+     * @dev Internal function that mints an amount of the token and assigns it to
+     * an account. This encapsulates the modification of balances such that the
+     * proper events are emitted.
+     * @param account The account that will receive the created tokens.
+     * @param value The amount that will be created.
+     */
+    function _mint(address account, uint256 value) internal {
+        require(account != address(0));
+
+        _totalSupply = _totalSupply.add(value);
+        _balances[account] = _balances[account].add(value);
+        emit Transfer(address(0), account, value);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# gtt
-GettCoin
+[![CircleCI](https://circleci.com/gh/SphereSoftware/SphereCoin/tree/master.svg?style=svg&circle-token=6b86b16ac67ac8362e96f17838aa4d093669be87)](https://circleci.com/gh/SphereSoftware/SphereCoin)
+
+## GettCoin Blockchain (Ethereum)
+
+This project represents GettCoin ERC20 smart contract. 
+
+## SphereCoin
+
+![SphereCoin](https://gettcoin.sphereinc.com/static/media/GettCoin_Mark.dccdd1b8.svg)
+
+Transfer coins within your company with ease, buy merchandise in the internal marketplace and motivate colleges for PRs
+
+## Slack
+
+Test slack workspace - [GettCoin](gettcoin.slack.com). 
+* [production](https://gettcoin.slack.com/apps/AQPR1A66M-gettcoin) app
+
+## Deployment 
+It was placed to `Ropsten` test network, you can check it here: 
+
+[0xaceda1c7b588be2c837c7bfb654633a663c4a472](https://ropsten.etherscan.io/address/0xaceda1c7b588be2c837c7bfb654633a663c4a472
+)
+
